e79e6a6a247ccee6bb77b7b59f1606492f68b601
オーサムライフ 株式会社AwesomeLife <osamuraifukabushikikaishaawesomelife@osamuraifunoMacBook.local>
2026-01-08 14:26:25 +0900

    Fix Stripe API version for Vercel build

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from 'stripe';
 
 export const stripe = process.env.STRIPE_SECRET_KEY
     ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-        apiVersion: '2025-01-27.acacia', // Use latest API version or pins
+        apiVersion: '2025-12-15.clover', // Update to match installed SDK type
         typescript: true,
     })
     : null;
